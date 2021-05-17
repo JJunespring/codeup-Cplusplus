e11ec8f7df1516ae523bbf16e8dc304215e20021
JJunespring <wjdwnsqja08@gmail.com>
2021-05-17 16:37:57 +0900

    [Hi]

---
+++ 1015.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	float a;
+	cin >> a;
+	cout << fixed;
+	cout << setprecision(2) << a;
+}
---
+++ 1018.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, c;
+	char b;
+
+	cin >> a >> b >> c;
+	cout << a << b << c;
+}
